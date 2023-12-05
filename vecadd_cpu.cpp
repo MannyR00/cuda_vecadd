feed2c43d71225a631ef0dbae18c7665c38fb730
MannyR00 <mannyramirez1330@gmail.com>
2023-12-05 14:12:19 -0800

    done

--- vecadd_cpu.cpp
+++ vecadd_cpu.cpp
@@ -2,29 +2,33 @@
 #include <math.h>
 #include <chrono>
 
-std::chrono::time_point<std::chrono::high_resolution_clock> start = std::chrono::high_resolution_clock::now();
+
 // function to add the elements of two arrays
 void add(int n, float *x, float *y)
 {
     for (int i = 0; i < n; i++)
         y[i] = x[i] + y[i];
 }
-std::chrono::time_point<std::chrono::high_resolution_clock> end = std::chrono::high_resolution_clock::now();
-std::chrono::duration<double> timePassed = end - start;
-
 
 int main(void)
 {
+
+
+
     int N = 1<<29; // 1M elements
 
     float *x = new float[N];
     float *y = new float[N];
+
+
 
     // initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
         x[i] = 1.0f;
         y[i] = 2.0f;
     }
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> start = std::chrono::high_resolution_clock::now();
 
     // Run kernel on 1M elements on the CPU
     add(N, x, y);
@@ -35,9 +39,16 @@ int main(void)
         maxError = fmax(maxError, fabs(y[i]-3.0f));
     std::cout << "Max error: " << maxError << std::endl;
 
+
+
     // Free memory
     delete [] x;
     delete [] y;
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> timePassed = end - start;
+
+    printf(" Elapsed time: %f seconds\n \n", timePassed.count());
 
     return 0;
 
